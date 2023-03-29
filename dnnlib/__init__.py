14f559026ece9e0df792531da12e4730ab38e51b
Thuan H. Nguyen <32274287+thuanz123@users.noreply.github.com>
2023-03-29 09:03:33 +0700

    Update __init__.py

--- dnnlib/__init__.py
+++ dnnlib/__init__.py
@@ -5,3 +5,5 @@
 # and any modifications thereto.  Any use, reproduction, disclosure or
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+from .util import EasyDict, make_cache_dir_path
